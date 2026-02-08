41f99001e7a32765f7ae0c0040893e545b8096e3
Mon <mon.pereira6029@gmail.com>
2026-02-08 12:34:12 +0100

    feat(game): add achievement domain class

---
+++ app/src/main/java/com/mon/gametracker/features/game/domain/achievement/Achievement.kt
@@ -0,0 +1,28 @@
+package com.mon.gametracker.features.game.domain.achievement
+
+import java.time.LocalDate
+
+data class Achievement(
+    val id: AchievementId,
+    val gameId: String,
+    val isCompleted: Boolean = false,
+    val completionDate: LocalDate? = null,
+    val guideURL: String
+) {
+    fun toggleCompletion() : Achievement {
+        return if (!isCompleted){
+            this.copy(
+                isCompleted = true,
+                completionDate = LocalDate.now()
+            )
+        } else {
+            this.copy(
+                isCompleted = false,
+                completionDate = null
+            )
+        }
+    }
+}
+
+@JvmInline
+value class AchievementId(val value: String)
