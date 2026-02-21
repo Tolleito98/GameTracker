b185c62ee785de46865314cd7f8bb17a0b4afb2d
Mon <mon.pereira6029@gmail.com>
2026-02-21 15:17:22 +0100

    refactor(game): Updated GameCard ui trash icon

--- app/src/main/java/com/mon/gametracker/features/game/ui/components/GameCard.kt
+++ app/src/main/java/com/mon/gametracker/features/game/ui/components/GameCard.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -48,7 +49,8 @@ fun GameCard(
         Row(
             modifier = modifier
                 .padding(8.dp)
-                .fillMaxWidth()
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
         ) {
 
             AsyncImage(
@@ -61,7 +63,9 @@ fun GameCard(
             )
             
             Column(
-                modifier = modifier.padding(start = 16.dp)
+                modifier = modifier
+                    .padding(start = 16.dp)
+                    .weight(1F)
             ) {
 
                 Text(
